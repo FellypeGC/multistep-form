70a79efd932b6bf15b4dcf5f32ff0ba6b165be9e
FellypeGC <gabrielfellypesteve@gmail.com>
2026-01-07 18:20:15 -0300

    Just implemented a component for steps displaying

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
 import UserForm from './components/UserForm'
 import ReviewForm from './components/ReviewForm'
 import Thanks from './components/Thanks'
+import Steps from './components/Steps'
 
 // Hooks
 import { useForm } from './hooks/useForm'
@@ -30,7 +31,7 @@ function App() {
         <p>Ficamos felizes com a sua compra. Utilize o formulário abaixo para avaliar o produto</p>
       </div>
       <div className="form-container">
-        <p>Passos</p>
+        <Steps currentStep={currentStep} />
         <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
           <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
