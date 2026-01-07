2c044c51dd485d72ce50ea616552b5850ac6709a
FellypeGC <gabrielfellypesteve@gmail.com>
2026-01-07 16:52:10 -0300

    form components were also added into the project, including a useForm hook for steps funcionalities

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,21 @@ import ReviewForm from './components/ReviewForm'
 import Thanks from './components/Thanks'
 
 // Hooks
+import { useForm } from './hooks/useForm'
 
 function App() {
+  const formComponents = [
+    <UserForm />,
+    <ReviewForm />,
+    <Thanks />
+  ]
+
+  const { 
+    currentStep,
+    currentComponent,
+    changeStep  
+  } = useForm(formComponents);
+
   return (
     <div className="app">
       <div className="header">
@@ -18,18 +31,16 @@ function App() {
       </div>
       <div className="form-container">
         <p>Passos</p>
-        <form>
-          <div className="inputs-container">
-            <UserForm />
-          </div>
+        <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
+          <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
-            <button>
+            <button type="button" onClick={() => changeStep(currentStep - 1)}>
               <span>
                 <GrFormPrevious />
                 Voltar
               </span>
             </button>
-            <button>
+            <button type="submit">
               <span>
                 Avançar
                 <GrFormNext />
