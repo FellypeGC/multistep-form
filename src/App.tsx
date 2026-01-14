c618e8b303c580f25ae0c0988710201c68c5968c
FellypeGC <gabrielfellypesteve@gmail.com>
2026-01-13 21:00:08 -0300

    feat: some tailwind classes added

--- src/App.tsx
+++ src/App.tsx
@@ -50,14 +50,16 @@ function App() {
   } = useForm(formComponents);
 
   return (
-    <div className="app">
-      <div className="header">
-        <h2>Deixe sua avaliação</h2>
+    <div className="p-8">
+      <div className="text-center mb-8">
+        <h2 className="text-[2rem] mb-8">Deixe sua avaliação</h2>
         <p>Ficamos felizes com a sua compra. Utilize o formulário abaixo para avaliar o produto</p>
       </div>
-      <div className="form-container">
+      <div className=" max-w-150 mx-auto bg-white p-6 shadow-[rgba(100,100,111,0.2)_0_7px_29px_0]">
         <Steps currentStep={currentStep} />
-        <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
+        <form
+          className="max-w-[125] mx-auto"
+          onSubmit={(e) => changeStep(currentStep + 1, e)}>
           <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
             <button type="button" onClick={() => changeStep(currentStep - 1)}>
