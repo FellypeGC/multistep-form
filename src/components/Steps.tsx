295421de9a6f887b0580587ac38736c1beff30af
FellypeGC <gabrielfellypesteve@gmail.com>
2026-01-15 18:26:37 -0300

    steps lang switched to english as well

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -10,18 +10,18 @@ const Steps = ({ currentStep }: stepsProps) => {
     <div className="relative mx-auto mb-8 flex items-center justify-between w-full max-w-[320px] after:content-[''] after:absolute after:top-6 after:left-0 after:w-full after:border-b after:border-[#ccc] sm:max-w-120">
       <div className={`relative z-10 flex flex-col items-center bg-white px-2 py-1 w-20 sm:w-30 ${currentStep >= 0 ? "font-bold text-[#7076F4]" : ""}`}>
         <AiOutlineUser className="text-2xl mb-1" />
-        <p className="text-xs sm:text-sm">Identificação</p>
+        <p className="text-xs sm:text-sm">Identification</p>
       </div>
 
       <div className={`relative z-10 flex flex-col items-center bg-white px-2 py-1 w-20 sm:w-30
         ${currentStep >= 1 ? "font-bold text-[#7076F4]" : ""}`}>
         <AiOutlineStar className="text-2xl mb-1" />
-        <p className="text-xs sm:text-sm">Avaliação</p>
+        <p className="text-xs sm:text-sm">Evaluation</p>
       </div>
 
       <div className={`relative z-10 flex flex-col items-center bg-white px-2 py-1 w-20 sm:w-30 ${currentStep >= 2 ? "font-bold text-[#7076F4]" : ""}`}>
         <FiSend className="text-2xl mb-1" />
-        <p className="text-xs sm:text-sm">Envio</p>
+        <p className="text-xs sm:text-sm">Send</p>
       </div>
     </div>
   )
