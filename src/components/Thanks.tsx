2ab3408ca427b4660fe976ec4904b8d84965e130
FellypeGC <gabrielfellypesteve@gmail.com>
2026-01-15 22:21:06 -0300

    feat: add random discount func

--- src/components/Thanks.tsx
+++ src/components/Thanks.tsx
@@ -30,11 +30,13 @@ type ThanksProps = {
 }
 
 const Thanks = ({ data }: ThanksProps) => {
+  let discount: number = Math.floor(Math.random() * 20 + 1);
+
   return (
     <div className="text-center">
       <h2 className="mb-4 font-medium">Almost done...</h2>
       <p className="text-[.9rem] mb-2 text-start">
-        Your feedback is very important to us {data.name}. Soon you will receive a coupon via email with a 7% discount coupon for your next purchase!
+        Your feedback is very important to us {data.name}. Soon you will receive a coupon via email with a {discount}% discount coupon for your next purchase!
       </p>
       <p className="text-[.9rem] mb-2 text-start">To complete your evaluation, please click the Send button below.</p>
       <h3 className="text-left my-4 mx-0">Here is the summary of your evaluation {data.name}:</h3>
